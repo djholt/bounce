91d4317e493c3f719773d833873a46f9503f02c7
D. J. Holt <djholt@cloakedhunter.com>
2012-10-09 18:35:47 -0600

    Initial Commit

---
+++ Bounce/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bounce
+//
+//  Created by D. J. Holt on 10/9/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
